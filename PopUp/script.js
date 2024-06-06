6ad65d2657a6442ff824232edef73de08f7d4f05
Syed Tajuddin Irfani <96831810+SyedTajuddinIrfani@users.noreply.github.com>
2024-06-06 13:45:18 +0500

    PopUp

---
+++ PopUp/script.js
@@ -0,0 +1,8 @@
+function showPopup(){
+    var popup=document.getElementById("popup");
+    popup.classList.add("hoja")
+}
+function closePopup(){
+    var popup=document.getElementById("popup");
+    popup.classList.remove("hoja")
+}
